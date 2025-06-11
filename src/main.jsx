4749975281ce0a05fb6ee13f517f63f6c83e9e37
AlexeyJW <kononenko.jw@gmail.com>
2025-06-11 17:30:52 +0300

    css add main

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Layout } from './components/Layout';
 import { BrowserRouter } from 'react-router-dom';
-import './index.css';
+import './style.css';
 
 // import { router } from './routes';
 // import { RouterProvider } from 'react-router-dom';
